1d034a867b24b272879c4c7ac543dc77e8313a9b
Uomar <uomarearlie7@gmail.com>
2019-06-21 10:54:42 +0300

    created app models

--- insta/models.py
+++ insta/models.py
@@ -1,6 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
 
-class image(models.Model):
+class Comments(models.Model):
+    review = models.CharField(max_length = 500)
+
+class Image(models.Model):
     image_name = models.CharField(max_length = 40)
     image_caption = models.TextField()
-    likes = models.
+    comments = models.ForeignKey(Comments)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to = 'images', default = 'gallery/media/images')
+    posted = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length = 50)
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length = 40)
+    last_name = models.CharField(max_length = 40)
+    name = models.OneToOneField(User,on_delete=models.CASCADE)
+    bio = models.TextField(max_length= 500, blank= True)
+    profile_pic = models.ImageField(upload_to = 'image', blank = True)
+
+    
+
+class Followers(models.Model):
+    name = models.ForeignKey(User)
+
+class Following(models.Model):
+    name = models.ForeignKey(User)
