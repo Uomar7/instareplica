e526a24e8e293a7387f981ca7d920f8ac262cdca
Uomar <uomarearlie7@gmail.com>
2019-06-24 14:13:10 +0300

    created forms for images, comments and profile

--- insta/forms.py
+++ insta/forms.py
@@ -1,8 +1,18 @@
 from django import forms
-from .models import Image,Profile
+from .models import Image,Profile,Comments
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        exclude = ["name"]
+        exclude = ["name","images"]
+    
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ["comments","user"]
+
+class CommentsForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        exclude = ["posted","posted_by"]
 
