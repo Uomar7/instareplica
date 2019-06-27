fb39d398ef548bff6703bb9728d9a73f48db9037
Uomar <uomarearlie7@gmail.com>
2019-06-27 08:25:23 +0300

    deployment to heroku.

--- instagram/settings.py
+++ instagram/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import django_heroku
 import dj_database_url
-import decouple import config
-import Csv
+from decouple import config,Csv
 
 MODE = config("MODE", default="dev")
 SECRET_KEY = config("SECRET_KEY")
